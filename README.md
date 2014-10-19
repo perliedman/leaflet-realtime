4d644b3dffb46254af8d296978ef8857ceaf2729
Per Liedman <per@liedman.net>
2014-10-19 22:00:24 +0200

    Push semantics

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ realtime.on('update', function() {
 
 ### Overview
 
-Leaflet Realtime reads and displays GeoJSON from a provided source. A "source" is usually a URL, and data can be fetched using AJAX (XHR), JSONP. Alternatively, you can write your own source, to provide data in just about any way you want.
+By default, Leaflet Realtime reads and displays GeoJSON from a provided source. A "source" is usually a URL, and data can be fetched using AJAX (XHR), JSONP. This means Leaflet Realtime will _poll_ for data, pulling it from the source. Alternatively, you can write your own source, to provide data in just about any way you want. Leaflet Realtime can also be made work with _push_ data, for example data pushed from the server using [socket.io](http://socket.io/) or similar.
 
 To be able to figure out when new features are added, when old features are removed, and which features are just updated, Leaflet Realtime needs to identify each feature uniquely. This is done using a _feature id_. Usually, this can be done using one of the feature's `properties`. By default, Leaflet Realtime will try to look for a called property `id` and use that.
 
@@ -34,6 +34,12 @@ Technically, `L.Realtime` extends `L.GeoJSON`, with the additional functionality
 Typical usage involves instantiating `L.Realtime` with options for [`style`](http://leafletjs.com/reference.html#geojson-style) and/or [`onEachFeature`](http://leafletjs.com/reference.html#geojson-oneachfeature), to customize styling and interaction, as well as adding a listener for the [`update`](#event-update) event, to for example list the features currently visible in the map.
 
 For ease of use and flexibility, Leaflet Realtime uses [reqwest](https://github.com/ded/reqwest) for getting data over HTTP(S), and it is bundled in the distribution.
+
+### Push data
+
+If you prefer getting data _pushed_ from the server, in contrast to Leaflet Realtime pulling it with a standard HTTP request, you can feed added and updated GeoJSON data to Leaflet Realtime using the `update` method. In this scenario, you will also need to remove features by explicit calls to `remove`.
+
+Since automatic updates do not make sense in a push scenario, you want to create the layer with the option `start` set to to `false`.
 
 ### API
 
@@ -71,8 +77,10 @@ Method                 | Returns        | Description
 `start()`              | `this`         | Starts automatic updates
 `stop()`               | `this`         | Stops automatic updates
 `isRunning()`          | `Boolean`      | Tells if automatic updates are running
-`update()`             | `this`         | Updates the layer's data (asynchronously)
+`update(<GeoJSON> featureData?)` | `this` | Updates the layer's data. If `featureData` is provided, it is used to add or update data in the layer, otherwise the layer's source is queried for new data asynchronously
+`remove(<GeoJSON> featureData)`  | `this` | Removes the provided feature or features from the layer
 `getLayer(<FeatureId> featureId)` | `ILayer` | Retrieves the layer used for a certain feature
+`getFeature(<FeatureId> featureId)` | `GeoJSON` | Retrieves the feature data for the given `featureId`
 
 #### <a name="source"></a> Source
 
--- src/L.Realtime.js
+++ src/L.Realtime.js
@@ -34,6 +34,7 @@ L.Realtime = L.GeoJSON.extend({
         }
 
         this._features = {};
+        this._featureLayers = {};
 
         if (this.options.start) {
             this.start();
@@ -63,26 +64,62 @@ L.Realtime = L.GeoJSON.extend({
         return this._timer;
     },
 
-    update: function() {
-        var responseHandler = L.bind(this._onNewData, this),
+    update: function(geojson) {
+        var responseHandler,
+            errorHandler;
+
+        if (geojson) {
+            this._onNewData(false, geojson);
+        } else {
+            responseHandler = L.bind(function(data) { this._onNewData(true, data); }, this);
             errorHandler = L.bind(this._onError, this);
 
-        this._src(responseHandler, errorHandler);
+            this._src(responseHandler, errorHandler);
+        }
+
+        return this;
+    },
+
+    remove: function(geojson) {
+        var features = L.Util.isArray(geojson) ? geojson : geojson.features ? geojson.features : [geojson],
+            exit = {},
+            i,
+            len,
+            fId;
+
+        for (i = 0, len = features.length; i < len; i++) {
+            fId = this.options.getFeatureId(features[i]);
+            this.removeLayer(this._featureLayers[fId]);
+            exit[fId] = this._features[fId];
+            delete this._features[fId];
+            delete this._featureLayers[fId];
+        }
+
+        this.fire('update', {
+            features: this._features,
+            enter: {},
+            update: {},
+            exit: exit
+        });
 
         return this;
     },
 
     getLayer: function(featureId) {
+        return this._featureLayers[featureId];
+    },
+
+    getFeature: function(featureId) {
         return this._features[featureId];
     },
 
-    _onNewData: function(response) {
+    _onNewData: function(removeMissing, response) {
         var oef = this.options.onEachFeature,
             layersToRemove = [],
             features = {},
             enter = {},
             update = {},
-            exit,
+            exit = {},
             i;
 
         this.options.onEachFeature = L.bind(function onEachFeature(f, l) {
@@ -95,8 +132,7 @@ L.Realtime = L.GeoJSON.extend({
             }
 
             fId = this.options.getFeatureId(f);
-            features[fId] = f;
-            oldLayer = this._features[fId];
+            oldLayer = this._featureLayers[fId];
 
             if (oldLayer) {
                 newLayer = this.options.updateFeature(f, oldLayer, l);
@@ -113,17 +149,21 @@ L.Realtime = L.GeoJSON.extend({
                 enter[fId] = f;
             }
 
-            this._features[fId] = l;
+            this._featureLayers[fId] = l;
+            this._features[fId] = features[fId] = f;
         }, this);
 
         this.addData(response);
-        exit = this._removeUnknown(features);
+
+        if (removeMissing) {
+            exit = this._removeUnknown(features);
+        }
         for (i = 0; i < layersToRemove.length; i++) {
             this.removeLayer(layersToRemove[i]);
         }
 
         this.fire('update', {
-            features: features,
+            features: this._features,
             enter: enter,
             update: update,
             exit: exit
@@ -142,10 +182,11 @@ L.Realtime = L.GeoJSON.extend({
     _removeUnknown: function(known) {
         var fId,
             removed = {};
-        for (fId in this._features) {
+        for (fId in this._featureLayers) {
             if (!known[fId]) {
-                this.removeLayer(this._features[fId]);
+                this.removeLayer(this._featureLayers[fId]);
                 removed[fId] = this._features[fId];
+                delete this._featureLayers[fId];
                 delete this._features[fId];
             }
         }
