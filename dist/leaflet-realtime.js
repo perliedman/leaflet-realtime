b54878cfa1bae88dbfd1ce11289f707b95beea83
Per Liedman <per@liedman.net>
2014-10-27 20:36:27 +0100

    Upgrade lib

--- dist/leaflet-realtime.js
+++ dist/leaflet-realtime.js
@@ -647,6 +647,7 @@ L.Realtime = L.GeoJSON.extend({
         }
 
         this._features = {};
+        this._featureLayers = {};
 
         if (this.options.start) {
             this.start();
@@ -676,27 +677,62 @@ L.Realtime = L.GeoJSON.extend({
         return this._timer;
     },
 
-    update: function() {
-        var responseHandler = L.bind(this._onNewData, this),
+    update: function(geojson) {
+        var responseHandler,
+            errorHandler;
+
+        if (geojson) {
+            this._onNewData(false, geojson);
+        } else {
+            responseHandler = L.bind(function(data) { this._onNewData(true, data); }, this);
             errorHandler = L.bind(this._onError, this);
 
-        this._src(responseHandler, errorHandler);
+            this._src(responseHandler, errorHandler);
+        }
+
+        return this;
+    },
+
+    remove: function(geojson) {
+        var features = L.Util.isArray(geojson) ? geojson : geojson.features ? geojson.features : [geojson],
+            exit = {},
+            i,
+            len,
+            fId;
+
+        for (i = 0, len = features.length; i < len; i++) {
+            fId = this.options.getFeatureId(features[i]);
+            this.removeLayer(this._featureLayers[fId]);
+            exit[fId] = this._features[fId];
+            delete this._features[fId];
+            delete this._featureLayers[fId];
+        }
+
+        this.fire('update', {
+            features: this._features,
+            enter: {},
+            update: {},
+            exit: exit
+        });
 
         return this;
     },
 
     getLayer: function(featureId) {
+        return this._featureLayers[featureId];
+    },
+
+    getFeature: function(featureId) {
         return this._features[featureId];
     },
 
-    _onNewData: function(response) {
+    _onNewData: function(removeMissing, response) {
         var oef = this.options.onEachFeature,
-            known = {},
             layersToRemove = [],
             features = {},
             enter = {},
             update = {},
-            exit,
+            exit = {},
             i;
 
         this.options.onEachFeature = L.bind(function onEachFeature(f, l) {
@@ -709,8 +745,7 @@ L.Realtime = L.GeoJSON.extend({
             }
 
             fId = this.options.getFeatureId(f);
-            features[fId] = f;
-            oldLayer = this._features[fId];
+            oldLayer = this._featureLayers[fId];
 
             if (oldLayer) {
                 newLayer = this.options.updateFeature(f, oldLayer, l);
@@ -727,18 +762,21 @@ L.Realtime = L.GeoJSON.extend({
                 enter[fId] = f;
             }
 
-            this._features[fId] = l;
-            known[fId] = true;
+            this._featureLayers[fId] = l;
+            this._features[fId] = features[fId] = f;
         }, this);
 
         this.addData(response);
-        exit = this._removeUnknown(known);
+
+        if (removeMissing) {
+            exit = this._removeUnknown(features);
+        }
         for (i = 0; i < layersToRemove.length; i++) {
             this.removeLayer(layersToRemove[i]);
         }
 
         this.fire('update', {
-            features: features,
+            features: this._features,
             enter: enter,
             update: update,
             exit: exit
@@ -757,10 +795,11 @@ L.Realtime = L.GeoJSON.extend({
     _removeUnknown: function(known) {
         var fId,
             removed = {};
-        for (fId in this._features) {
+        for (fId in this._featureLayers) {
             if (!known[fId]) {
-                this.removeLayer(this._features[fId]);
+                this.removeLayer(this._featureLayers[fId]);
                 removed[fId] = this._features[fId];
+                delete this._featureLayers[fId];
                 delete this._features[fId];
             }
         }
@@ -773,7 +812,10 @@ L.realtime = function(src, options) {
     return new L.Realtime(src, options);
 };
 
+L.Realtime.reqwest = reqwest;
+
 module.exports = L.Realtime;
+
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"reqwest":1}]},{},[2])(2)
 });
