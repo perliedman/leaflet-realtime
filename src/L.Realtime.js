perliedman/leaflet-realtime

commit 98bd41f8cee7cc6a92923e86f854f0e5611a6c12

Added removeMissing options when you want to manage feature removal by yourself. Added automated start/stop on map layer addition/removal

src/L.Realtime.js
@@ -17,7 +17,8 @@ L.Realtime = L.Layer.extend({
             }
         },
         logErrors: true,
-        cache: false
+        cache: false,
+        removeMissing: true
     },
 
     initialize: function(src, options) {
@@ -34,10 +35,6 @@ L.Realtime = L.Layer.extend({
         this._features = {};
         this._featureLayers = {};
         this._requestCount = 0;
-
-        if (this.options.start) {
-            this.start();
-        }
     },
 
     start: function() {
@@ -87,7 +84,7 @@ L.Realtime = L.Layer.extend({
         if (geojson) {
             this._onNewData(false, geojson);
         } else {
-            responseHandler = L.bind(function(data) { this._onNewData(true, data); }, this);
+            responseHandler = L.bind(function(data) { this._onNewData(this.options.removeMissing, data); }, this);
             errorHandler = L.bind(this._onError, this);
 
             this._src(checkRequestCount(responseHandler), checkRequestCount(errorHandler));
@@ -140,9 +137,11 @@ L.Realtime = L.Layer.extend({
 
     onAdd: function(map) {
         map.addLayer(this._container);
+        this.start();
     },
 
     onRemove: function(map) {
+        this.stop();
         map.removeLayer(this._container);
     },
 

dist/leaflet-realtime.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.L || (g.L = {})).Realtime = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.L || (g.L = {})).Realtime = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
 L.Realtime = L.Layer.extend({
@@ -189,6 +189,11 @@ L.Realtime = L.Layer.extend({
                 if (options.onEachFeature) {
                     options.onEachFeature(geojson, layer);
                 }
+
+                if (options.style && layer.setStyle) {
+                    layer.setStyle(options.style(geojson));
+                }
+
             }
 
             layer.feature = f;

commit 697675c8b7eae67d63dc09d1ac59152ac3669e24

Add option onlyRunWhenAdded, removing regression from 98bd41f8cee7cc6a92923e86f854f0e5611a6c12
Fixes #139

src/L.Realtime.js
@@ -18,7 +18,8 @@ L.Realtime = L.Layer.extend({
         },
         logErrors: true,
         cache: false,
-        removeMissing: true
+        removeMissing: true,
+        onlyRunWhenAdded: false
     },
 
     initialize: function(src, options) {
@@ -35,6 +36,10 @@ L.Realtime = L.Layer.extend({
         this._features = {};
         this._featureLayers = {};
         this._requestCount = 0;
+
+        if (this.options.start && !this.options.onlyRunWhenAdded) {
+            this.start();
+        }
     },
 
     start: function() {
@@ -143,7 +148,10 @@ L.Realtime = L.Layer.extend({
     },
 
     onRemove: function(map) {
-        this.stop();
+        if (this.options.onlyRunWhenAdded) {
+            this.stop();
+        }
+        
         map.removeLayer(this._container);
     },
 